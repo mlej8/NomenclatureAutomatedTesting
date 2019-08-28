02019b6481dfdc8a7bd7534b025c945a9fd363f0
Michael Li <er.li@mail.mcgill.ca>
2019-08-28 17:30:29 -0400

    gitignore test-ouput/ and target/ folders trying to implement unit tests

--- src/main/java/ca/gc/pch/test/nomenclature/config/UserConfiguration.java
+++ src/main/java/ca/gc/pch/test/nomenclature/config/UserConfiguration.java
@@ -7,14 +7,15 @@ import javax.swing.JOptionPane;
 
 public class UserConfiguration {
 	/* This class contains the UserConfigurations variables */
-	private String testEnv; // stores the environment 
-	private int isFluent;   // stored the run mode 
+	private String testEnv = null; // stores the environment 
+	private int isFluent = -1;     // stored the run mode 
 	
 	public UserConfiguration(){}
 	
 	public void configureTestEnvironment() {
 		/* Environment */
-		// Creating environment Map  
+		// Creating environment Map if it is null
+		if (this.testEnv == null) {
 		String[] environment = {"DEV", "STG", "PROD"};
 		String[] urls = {"https://dev.", "https://stg.", "https://www."};
 		Map<String, String> envMap = new HashMap<String,String>();
@@ -34,12 +35,15 @@ public class UserConfiguration {
 		
 		// Get URL associated with testing environment
 		this.testEnv = (envMap.get(environment[index]));
+		}
 	}
 	
 	
 	public void configureTestMode() {
-	/* Fluent mode or not, Please keep in mind that every time Selenium tests run, it uses a brand new browser.	 */        
-	String[] options = {"Fluent", "Break"}; // creating options to chose how testing is ran  
+		if (this.isFluent == -1) {
+
+	// Creating options to chose how testing is ran
+	String[] options = {"Fluent", "Break"};   
 
 	// If Fluent is clicked, the value of isFluent will be 0, if Break is choosen, then the value of isFluent will be 1
 	this.isFluent = JOptionPane.showOptionDialog(null, 												   // Component parentComponent
@@ -50,6 +54,7 @@ public class UserConfiguration {
 			null,																					   // Icon icon
 			options,																				   // Object[] options
 			options[0]);
+		}
 	}
 
 	public int getIsFluent() {
@@ -62,5 +67,16 @@ public class UserConfiguration {
 
 	public String getTestEnv() {
 		return testEnv;
+	}
+	
+	public void setTestEnv(String testEnv) {
+		testEnv = testEnv.toUpperCase();
+		String[] environment = {"DEV", "STG", "PROD"};
+		String[] urls = {"https://dev.", "https://stg.", "https://www."};
+		Map<String, String> envMap = new HashMap<String,String>();
+		for(int i = 0; i < urls.length; i ++) {
+			envMap.put(environment[i], urls[i]);
+		}
+		this.testEnv = envMap.get(testEnv);
 	}
 }
--- src/test/java/ca/gc/pch/test/nomenclature/testcases/BrowsePageTest.java
+++ src/test/java/ca/gc/pch/test/nomenclature/testcases/BrowsePageTest.java
@@ -15,29 +15,46 @@ public class BrowsePageTest extends TestBase {
 		
 	@BeforeMethod
 	public void setUp() {
-		initialization(); // initialize driver, implicit wait, actions, etc.
+		initialization(); // initialize driver, implicit wait, actions, etc. We are using a fresh new Browser for every test cases in order to avoid having cached too many cookies or browser crashing/dying due to running out of memory or other reasons  
 		browsePage = new BrowsePage(); // initialize object of BrowsePage class
 	}
 	
-	@Test(priority=1)
-	public void browsePageTitle() {
+	// Test cases are all INDEPENDENT from each other.
+	@Test(priority=1, groups="Title")
+	public void browsePageTitleTest() {
 		String title = browsePage.validateBrowsePageTitle();
 		Assert.assertEquals(title, "Parcourir la hiérarchie - Nomenclature");
 	}
 	
-	@Test(priority=2)
-	public void browsePageDefaultLanguage() {
+	@Test(priority=2, groups="Default Parameter")
+	public void browsePageDefaultLanguageTest() {
 		String language = NomenclatureUtil.getLanguage();
 		Assert.assertEquals(language, "Français");
 	}
 	
-	@Test(priority=3)
+	@Test(priority=3, groups="Default Parameter", dependsOnMethods="browsePageDefaultLanguageTest")
+	public void browsePageDefaultTermOrderTest() {
+		String termOrder = browsePage.getTermOrder();
+		Assert.assertEquals(termOrder, "Inversé");
+	}
+	
+	@Test(priority=4, groups="Default Parameter")
+	public void browsePageDefaultLinguisticVariantTest() {
+		String linguisticVariant = browsePage.getLinguisticVariant();
+		Assert.assertEquals(linguisticVariant, "International");
+	}
+	
+	@Test(priority=5, groups="Structure Tree Order")
+	public void categoriesOrder() {
+		Assert.assertTrue(browsePage.isCategoryOrdered());
+	}
+	
+	@Test(priority=6, groups="Structure Tree Order")
 	public void subTreeOrder() {
 		browsePage.navigateCategory("Catégorie 01",false);
 		boolean order = browsePage.isSubTreeOrdered();
 		Assert.assertTrue(order);
 	}
-	
 	
 	@AfterMethod
 	public void cleanUp() {
